0bf7e702f3929a4641820ca867d96490fb76ead4
greatakash <greatakash@users.noreply.github.com>
2016-09-02 00:49:52 +0530

    mail attachment
    mail attachment

---
+++ attachmentToMail.php
@@ -0,0 +1,17 @@
+<?php
+require 'include/class.phpmailer.php';
+$email = new PHPMailer();
+$email->From      = 'rajakashxxx@gmail.com';
+$email->FromName  = 'Your Name';
+$email->Subject   = 'Message Subject';
+$bodytext = "hello" ;
+$email->Body      = $bodytext;
+$email->AddAddress( 'akashr@domain.com');
+
+ $file_to_attach = 'anyfile.txt';
+
+$email->AddAttachment( $file_to_attach , 'anyfile.txt' );
+echo "mail sent" ;
+return $email->Send();
+
+?>
